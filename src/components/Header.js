ebaa21100b96e72558dc2526aa18d3cd9ceac9b5
mrjoshdev <wyman.josh@gmail.com>
2019-01-12 12:37:47 -0500

    CSS styling for home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,12 @@ import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 const Header = () => (
   <header>
       <h1>Collective Engine</h1>
+      <div class="home-link">
       <NavLink to="/" activeClassName="is-active" exact={true}>Home  </NavLink>
       <NavLink to="/NewCampaign" activeClassName="is-active">New Campaign  </NavLink>
       <NavLink to="/NewUser" activeClassName="is-active">Sign In</NavLink>
+      </div>
+
   </header>
 )
 
