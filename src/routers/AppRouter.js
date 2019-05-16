d7dbe584da07e7288d7125313af850a446cbed91
Daniel schwartz <mdblakestj@gmail.com>
2019-05-16 19:34:51 -0400

    fixed bootstrap

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,35 +1,38 @@
-import React from 'react';
-import { Router, Route, Switch, Link, NavLink} from 'react-router-dom';
-import Header from './../components/Header.js'
-import NewCampaign from '../components/NewCampaign.js';
-import NewUser from '../components/NewUser.js';
+import React from "react";
+import { Router, Route, Switch, Link, NavLink } from "react-router-dom";
+import Header from "./../components/Header.js";
+import NewCampaign from "../components/NewCampaign.js";
+import NewUser from "../components/NewUser.js";
 // import SignIn from './components/SignIn.js';
-import Home from '../components/Home.js';
-import Campaign from '../components/Campaign';
-import LogIn from '../components/Login';
-import createHistory from 'history/createBrowserHistory';
-import PrivateRoute from './PrivateRoute'
-import MyCampaigns from '../components/MyCampaigns'
-import JoinEngine from '../components/JoinEngine'
+import Home from "../components/Home.js";
+import Campaign from "../components/Campaign";
+import LogIn from "../components/Login";
+// import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from "history";
+import PrivateRoute from "./PrivateRoute";
+import MyCampaigns from "../components/MyCampaigns";
+import JoinEngine from "../components/JoinEngine";
+import { Container, Row, Col } from "react-bootstrap";
 
-export const history = createHistory();
+export const history = createBrowserHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
       <Header />
-      <Switch>
-        <Route path="/" component={LogIn} exact={true} />
-        <Route path="/Home" component={Home}/>
-        <PrivateRoute path="/NewCampaign" component={NewCampaign}/>
-        <Route path="/MyCampaigns" component={MyCampaigns}/>
-        <Route path="/campaign/:id" component={Campaign} />
-        <Route path="/NewUser" component={NewUser}/>
-        <Route path="/JoinEngine/:id" component={JoinEngine} />
-
-      </Switch>
+      <Container>
+        <Switch>
+          <Route path="/" component={LogIn} exact={true} />
+          <Route path="/Home" component={Home} />
+          <PrivateRoute path="/NewCampaign" component={NewCampaign} />
+          <Route path="/MyCampaigns" component={MyCampaigns} />
+          <Route path="/campaign/:id" component={Campaign} />
+          <Route path="/NewUser" component={NewUser} />
+          <Route path="/JoinEngine/:id" component={JoinEngine} />
+        </Switch>
+      </Container>
     </div>
   </Router>
-)
+);
 
 export default AppRouter;
